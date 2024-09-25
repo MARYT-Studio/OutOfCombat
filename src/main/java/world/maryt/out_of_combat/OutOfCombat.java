e07e5a9bd30ab817a57e9baed3dfd3d5a32c758e
RisingInIris2017 <risinginiris2017@gmail.com>
2024-09-25 21:41:13 +0800

    Compat all 1.18.x

--- src/main/java/world/maryt/out_of_combat/OutOfCombat.java
+++ src/main/java/world/maryt/out_of_combat/OutOfCombat.java
@@ -1,18 +1,18 @@
 package world.maryt.out_of_combat;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import world.maryt.out_of_combat.handler.OutOfCombatHandler;
 
 @Mod(OutOfCombat.MODID)
 public class OutOfCombat {
 
     public static final String MODID = "out_of_combat";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger(OutOfCombat.MODID);
 
     public OutOfCombat() {
         // Event handlers' registrations must be in the constructor of the main class
